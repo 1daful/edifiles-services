8528d1059634130de91a883346a7428f8786fd09
1daful <wonderayanfe0@gmail.com>
2023-08-25 06:11:21 +0100

    Add view resolvers

--- src/utility/apiClient.ts
+++
@@ -1,8 +0,0 @@
-//import { MediaType } from "./Types";
-
-//import { Resource } from "./api/Resource";
-export interface ApiClient{
-    //load(file: string): any;
-    get(params: Record<string,any>): any;
-    post(/*data: Record<string,any>, */params: Record<string, any>, auth?: any): any;
-}
